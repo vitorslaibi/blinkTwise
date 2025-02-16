b6a992333b3473189b2888af171b5f23c13ba842
“Vitor <vitor.slaibi@yahoo.com>
2025-02-16 16:18:54 -0600

    import db

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
 from app.models import User
 from app.forms import LoginForm, RegistrationForm
+from app import db  # Import the db object
 
 # Create blueprints
 main_routes = Blueprint('main', __name__)
